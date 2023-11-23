d3e6afd367318598a00073ed12f05f84689be391
Ted-Kelvin-Mutwiri <tedkelvin19@gmail.com>
2023-11-23 11:57:40 +0300

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_series = []
+    if length == 0:
+        fibonacci_series = []
+    elif length == 1:
+        fibonacci_series = [0]
+    elif length == 2:
+        fibonacci_series = [0, 1]
+    else:
+        fibonacci_series = [0, 1]
+        for i in range(2, length):
+            fibonacci_series.append(fibonacci_series[i-1] + fibonacci_series[i-2])
+    print(fibonacci_series)
+print_fibonacci(9)        
